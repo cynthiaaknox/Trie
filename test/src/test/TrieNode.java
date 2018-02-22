e2012e8c3bb457f3ef4444ee2985c8843a3aeec5
Cynthia Knox <caknox@CAKNOX-US.us.oracle.com>
2018-02-22 10:56:40 -0700

    Committing final additions before testing.

--- test/src/test/TrieNode.java
+++ test/src/test/TrieNode.java
@@ -9,4 +9,11 @@ public class TrieNode {
     
     char c;
     HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+    boolean isLeaf;
+    
+    public TrieNode() {}
+    
+    public TrieNode(char c) {
+        this.c = c;
+    }
 }
