bfb032103f91b4cddebccdc77e5f7153976cd9cc
Cynthia Knox <caknox@CAKNOX-US.us.oracle.com>
2018-02-22 10:30:25 -0700

    Adding in Trie.java

---
+++ test/src/test/Trie.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * https://www.programcreek.com/2014/05/leetcode-implement-trie-prefix-tree-java/
+ */
+public class Trie {
+    private TrieNode root;
+    
+    public Trie() {
+        root = new TrieNode();
+    }
+}
